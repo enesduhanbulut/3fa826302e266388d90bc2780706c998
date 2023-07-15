57086db9a91e43e7870d066778c70a1bdbc656d8
Enes Duhan Bulut <dev.duhanbulut@gmail.com>
2023-07-15 16:45:11 +0300

    feat: implemented loading bar interactor but tested yet

---
+++ app/src/main/java/com/duhan/satelliteinfo/features/core/presantation/LoadingBarInteractor.kt
@@ -0,0 +1,59 @@
+package com.duhan.satelliteinfo.features.core.presantation
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
+
+interface LoadingBarInteractor<US> {
+    fun registerLoadingBar(
+        supportFragmentManager: FragmentManager,
+        loadingState: US,
+        viewDataBinding: ViewDataBinding,
+        loadingBarContainerId: Int
+    )
+
+    fun onStateChange(uiState: US)
+}
+
+class LoadingBarInteractorImpl<US> : LoadingBarInteractor<US> {
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ViewDataBinding
+    private var loadingBarContainerId: Int = 0
+    private var loadingState: US? = null
+    override fun onStateChange(uiState: US) {
+        if (loadingState == null) return
+        if (loadingBarContainerId == 0) return
+        val fragmentContainerView = binding.root.findViewById<View>(loadingBarContainerId)
+        if (fragmentContainerView !is FragmentContainerView) return
+        with(fragmentContainerView) {
+            if (loadingState == uiState) {
+                visibility = View.VISIBLE
+                if (parent != null) {
+                    fragmentManager.putFragment(
+                        Bundle(),
+                        "loadingBar",
+                        LoadingFragment()
+                    )
+                }
+            } else {
+                fragmentContainerView.visibility = View.GONE
+                fragmentManager.popBackStack()
+            }
+        }
+    }
+
+    override fun registerLoadingBar(
+        supportFragmentManager: FragmentManager,
+        loadingState: US,
+        viewDataBinding: ViewDataBinding,
+        loadingBarContainerId: Int
+    ) {
+        this.fragmentManager = supportFragmentManager
+        this.loadingState = loadingState
+        this.binding = viewDataBinding
+        this.loadingBarContainerId = loadingBarContainerId
+    }
+
+}
---
+++ app/src/main/java/com/duhan/satelliteinfo/features/core/presantation/LoadingFragment.kt
@@ -0,0 +1,18 @@
+package com.duhan.satelliteinfo.features.core.presantation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.duhan.satelliteinfo.R
+
+class LoadingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
+
+}
